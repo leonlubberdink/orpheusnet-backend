14f1e6070e06cbc6310259554c38c40633e7fe66
leonlubberdink <mail@leonlubberdink.com>
2023-07-10 15:39:03 +0200

    Added different error functions (dev and prod) in error controller

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,35 @@
+// Function that gets triggered on error in development
+const sendErrDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+// Function that gets triggered on error in production
+const sendErrProd = (err, res) => {
+  //Operational, trusted error: send message to client
+  if (err.isOperational)
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+  //Programming or other unknown error: don't leak error details to client
+  console.error('ERROR', sendErrProd);
+  res.status(500).json({
+    status: 'error',
+    message: 'Something went wrong! Please try again',
+  });
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') sendErrDev(err, res);
+
+  if (process.env.NODE_ENV === 'production') sendErrProd(err, res);
 };
