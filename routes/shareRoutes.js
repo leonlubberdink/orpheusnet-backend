9d727785f8ac8e37d8333eae1c40c43c2a7f5f36
leonlubberdink <mail@leonlubberdink.com>
2023-07-11 17:23:25 +0200

    Implemented restrict to roles middleware, part 2

--- routes/shareRoutes.js
+++ routes/shareRoutes.js
@@ -4,8 +4,8 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-//Restrict non auth user routes to admin
-router.use(authController.protect);
+//Restrict non "auth user" routes to admin
+router.use(authController.protect, authController.restrictTo('admin'));
 
 // Routes for creating a new share and requesting all shares with filter
 router
