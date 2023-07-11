6c9463ae8006d67b42d139d662fbb7452d1f90b6
leonlubberdink <mail@leonlubberdink.com>
2023-07-11 11:29:48 +0200

    Implemented Signup User functionality and auth  with JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,25 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
-const factory = require('./controllerFactory');
+const jwt = require('jsonwebtoken');
 
-exports.signup = factory.createOne(User);
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    userName: req.body.userName,
+    email: req.body.email,
+    userImage: req.body.userImage,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  res.status(202).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser,
+    },
+  });
+});
