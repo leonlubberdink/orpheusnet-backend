3c58722040f37506dd5f25f9d4c18a6aca6a85c6
leonlubberdink <mail@leonlubberdink.com>
2023-07-12 12:50:01 +0200

    JWT now gets stored in secure cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,21 @@ const signToken = (id) => {
 const createAndSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  // cookieOptions.secure only works wenn using prowser in production
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from response output
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
@@ -25,7 +40,6 @@ const createAndSendToken = (user, statusCode, res) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-  console.log(req.body);
   const newUser = await User.create({
     userName: req.body.userName,
     email: req.body.email,
