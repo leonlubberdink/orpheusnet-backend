6edc46b67f6ed35ad9ef44b6160421097196d956
leonlubberdink <mail@leonlubberdink.com>
2023-09-07 15:58:08 +0200

    Added Git CI/CD Workflow

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 //Require node modules
-const path = require('path');
+const path = require("path");
 
 //Require installed packages
-const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 //Require custom modules
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
-const userRouter = require('./routes/userRoutes');
-const shareRouter = require('./routes/shareRoutes');
-const groupRouter = require('./routes/groupRoutes');
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const userRouter = require("./routes/userRoutes");
+const shareRouter = require("./routes/shareRoutes");
+const groupRouter = require("./routes/groupRoutes");
 
 //Start express app
 const app = express();
 
+app.get("/test", (_req, res) => {
+  res.status(200).send("Hello world");
+});
+
 // 1 GLOBAL MIDDLEWARES
 
 // MIDDLEWARE FOR DEVELOPMEN ENV
-if (process.env.NODE_ENV === 'development') {
-  console.log('DEVELOPMENT ENVIRONMENT');
+if (process.env.NODE_ENV === "development") {
+  console.log("DEVELOPMENT ENVIRONMENT");
 
   // Morgan Middleware for logging request info to console.
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
 
   // // Custom, Middleware for testing purposes
   // app.use((req, res, next) => {
@@ -38,49 +42,49 @@ if (process.env.NODE_ENV === 'development') {
 
 // MIDDLEWARES FOR PRODFUCTION ENV
 // Serving static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Set security https headers
 app.use(
   helmet({
     contentSecurityPolicy: {
       directives: {
-        defaultSrc: ["'self'", 'data:', 'blob:', 'https:', 'ws:'],
+        defaultSrc: ["'self'", "data:", "blob:", "https:", "ws:"],
         baseUri: ["'self'"],
-        fontSrc: ["'self'", 'https:', 'data:'],
+        fontSrc: ["'self'", "https:", "data:"],
         scriptSrc: [
           "'self'",
-          'https:',
-          'http:',
-          'blob:',
-          'https://*.mapbox.com',
-          'https://js.stripe.com',
-          'https://m.stripe.network',
+          "https:",
+          "http:",
+          "blob:",
+          "https://*.mapbox.com",
+          "https://js.stripe.com",
+          "https://m.stripe.network",
         ],
-        frameSrc: ["'self'", 'https://js.stripe.com'],
+        frameSrc: ["'self'", "https://js.stripe.com"],
         objectSrc: ["'none'"],
-        styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
+        styleSrc: ["'self'", "https:", "'unsafe-inline'"],
         workerSrc: [
           "'self'",
-          'data:',
-          'blob:',
-          'https://*.tiles.mapbox.com',
-          'https://api.mapbox.com',
-          'https://events.mapbox.com',
-          'https://m.stripe.network',
+          "data:",
+          "blob:",
+          "https://*.tiles.mapbox.com",
+          "https://api.mapbox.com",
+          "https://events.mapbox.com",
+          "https://m.stripe.network",
         ],
-        childSrc: ["'self'", 'blob:'],
-        imgSrc: ["'self'", 'data:', 'blob:'],
+        childSrc: ["'self'", "blob:"],
+        imgSrc: ["'self'", "data:", "blob:"],
         formAction: ["'self'"],
         connectSrc: [
           "'self'",
           "'unsafe-inline'",
-          'data:',
-          'blob:',
-          'https://*.stripe.com',
-          'https://*.mapbox.com',
-          'https://bundle.js:*',
-          'ws://localhost:*/',
+          "data:",
+          "blob:",
+          "https://*.stripe.com",
+          "https://*.mapbox.com",
+          "https://bundle.js:*",
+          "ws://localhost:*/",
         ],
         upgradeInsecureRequests: [],
       },
@@ -92,13 +96,13 @@ app.use(
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour',
+  message: "Too many requests from this IP, please try again in an hour",
 });
 
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 // Parse incoming requests with JSON payloads (body-parser)
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -109,17 +113,17 @@ app.use(xss());
 // Preventg parameter polution
 app.use(
   hpp({
-    whitelist: ['format', 'shareType'],
+    whitelist: ["format", "shareType"],
   })
 );
 
 // 2 ROUTES
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/shares', shareRouter);
-app.use('/api/v1/groups', groupRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/shares", shareRouter);
+app.use("/api/v1/groups", groupRouter);
 
 // Create AppError wenn non existing route is used
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
   console.log(err.name, err.message);
   process.exit(1);
 });
 
-dotenv.config({ path: './.env' });
-const app = require('./app');
+dotenv.config({ path: "./.env" });
+const app = require("./app");
 
 const database = process.env.DATABASE.replace(
-  '<PASSWORD>',
+  "<PASSWORD>",
   process.env.DATABASE_PASSWORD
-).replace('<DATABASE_NAME>', process.env.DATABASE_NAME);
+).replace("<DATABASE_NAME>", process.env.DATABASE_NAME);
 
 mongoose
   .connect(database, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then((con) => console.log('DB Connection successful!'));
+  .then((con) => console.log("DB Connection successful!"));
 
 const port = process.env.PORT || 7999;
 const server = app.listen(port, () => {
@@ -28,8 +28,8 @@ const server = app.listen(port, () => {
 });
 
 // Handle unhandled rejected promisses
-process.on('unhandledRejection', (err) => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
