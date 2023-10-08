a7a402120be6a11fb1d3a9337c1219ad2afe1f8b
leonlubberdink <mail@leonlubberdink.com>
2023-10-08 17:44:37 +0200

    Moved CORS back to app

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
+const cors = require("cors");
 
 //Require custom modules
 const AppError = require("./utils/appError");
@@ -44,6 +45,9 @@ if (process.env.NODE_ENV === "development") {
 // MIDDLEWARES FOR PRODUCTION ENV
 // Serving static files
 app.use(express.static(path.join(__dirname, "public")));
+
+///USE CORS
+app.use(cors());
 
 // Set security https headers
 app.use(
--- routes/soundcloudRoutes.js
+++ routes/soundcloudRoutes.js
@@ -1,8 +1,4 @@
 const express = require("express");
-const cors = require("cors");
-
-///USE CORS
-express.use(cors());
 
 const widgetController = require("../controllers/widgetController");
 
