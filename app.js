288b55f1c402a0f6273141b515dc4cb6cb994741
leonlubberdink <mail@leonlubberdink.com>
2023-11-01 13:45:02 +0100

    changed rate limit setting

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 //Require node modules
-const path = require("path");
+const path = require('path');
 
 //Require installed packages
-const express = require("express");
-const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const hpp = require("hpp");
-const cors = require("cors");
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
 
 //Require custom modules
-const AppError = require("./utils/appError");
-const globalErrorHandler = require("./controllers/errorController");
-const userRouter = require("./routes/userRoutes");
-const shareRouter = require("./routes/shareRoutes");
-const groupRouter = require("./routes/groupRoutes");
-const soundcloudRouter = require("./routes/soundcloudRoutes");
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const userRouter = require('./routes/userRoutes');
+const shareRouter = require('./routes/shareRoutes');
+const groupRouter = require('./routes/groupRoutes');
+const soundcloudRouter = require('./routes/soundcloudRoutes');
 
 //Start express app
 const app = express();
 
-app.get("/test", (_req, res) => {
-  res.status(200).send("Hello world!");
+app.get('/test', (_req, res) => {
+  res.status(200).send('Hello world!');
 });
 
 // 1 GLOBAL MIDDLEWARES
 
 // MIDDLEWARE FOR DEVELOPMEN ENV
-if (process.env.NODE_ENV === "development") {
-  console.log("DEVELOPMENT ENVIRONMENT");
+if (process.env.NODE_ENV === 'development') {
+  console.log('DEVELOPMENT ENVIRONMENT');
 
   // Morgan Middleware for logging request info to console.
-  app.use(morgan("dev"));
+  app.use(morgan('dev'));
 
   // // Custom, Middleware for testing purposes
   // app.use((req, res, next) => {
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === "development") {
 
 // MIDDLEWARES FOR PRODUCTION ENV
 // Serving static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 ///USE CORS
 app.use(cors());
@@ -54,42 +54,42 @@ app.use(
   helmet({
     contentSecurityPolicy: {
       directives: {
-        defaultSrc: ["'self'", "data:", "blob:", "https:", "ws:"],
+        defaultSrc: ["'self'", 'data:', 'blob:', 'https:', 'ws:'],
         baseUri: ["'self'"],
-        fontSrc: ["'self'", "https:", "data:"],
+        fontSrc: ["'self'", 'https:', 'data:'],
         scriptSrc: [
           "'self'",
-          "https:",
-          "http:",
-          "blob:",
-          "https://*.mapbox.com",
-          "https://js.stripe.com",
-          "https://m.stripe.network",
+          'https:',
+          'http:',
+          'blob:',
+          'https://*.mapbox.com',
+          'https://js.stripe.com',
+          'https://m.stripe.network',
         ],
-        frameSrc: ["'self'", "https://js.stripe.com"],
+        frameSrc: ["'self'", 'https://js.stripe.com'],
         objectSrc: ["'none'"],
-        styleSrc: ["'self'", "https:", "'unsafe-inline'"],
+        styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
         workerSrc: [
           "'self'",
-          "data:",
-          "blob:",
-          "https://*.tiles.mapbox.com",
-          "https://api.mapbox.com",
-          "https://events.mapbox.com",
-          "https://m.stripe.network",
+          'data:',
+          'blob:',
+          'https://*.tiles.mapbox.com',
+          'https://api.mapbox.com',
+          'https://events.mapbox.com',
+          'https://m.stripe.network',
         ],
-        childSrc: ["'self'", "blob:"],
-        imgSrc: ["'self'", "data:", "blob:"],
+        childSrc: ["'self'", 'blob:'],
+        imgSrc: ["'self'", 'data:', 'blob:'],
         formAction: ["'self'"],
         connectSrc: [
           "'self'",
           "'unsafe-inline'",
-          "data:",
-          "blob:",
-          "https://*.stripe.com",
-          "https://*.mapbox.com",
-          "https://bundle.js:*",
-          "ws://localhost:*/",
+          'data:',
+          'blob:',
+          'https://*.stripe.com',
+          'https://*.mapbox.com',
+          'https://bundle.js:*',
+          'ws://localhost:*/',
         ],
         upgradeInsecureRequests: [],
       },
@@ -100,14 +100,14 @@ app.use(
 // Rate limiting
 const limiter = rateLimit({
   max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour",
+  windowMs: 10 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour',
 });
 
-app.use("/api", limiter);
+// app.use('/api', limiter);
 
 // Parse incoming requests with JSON payloads (body-parser)
-app.use(express.json({ limit: "10kb" }));
+app.use(express.json({ limit: '10kb' }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -118,18 +118,18 @@ app.use(xss());
 // Preventg parameter polution
 app.use(
   hpp({
-    whitelist: ["format", "shareType"],
+    whitelist: ['format', 'shareType'],
   })
 );
 
 // 2 ROUTES
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/shares", shareRouter);
-app.use("/api/v1/groups", groupRouter);
-app.use("/api/v1/soundcloud", soundcloudRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/shares', shareRouter);
+app.use('/api/v1/groups', groupRouter);
+app.use('/api/v1/soundcloud', soundcloudRouter);
 
 // Create AppError wenn non existing route is used
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
