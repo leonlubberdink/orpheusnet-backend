453b503d460891a12e4da1b716c33b5ce25c5c7b
Leon Lubberdink <lubberdink@strato.de>
2024-01-26 12:27:23 +0100

    Resolved merge conflict in authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -243,22 +243,22 @@ exports.verifyJWT = catchAsync(async (req, res, next) => {
   next();
 });
 
-exports.protect = catchAsync(async (req, res, next) => {
-  // 1) Getting token and check if it's there
-  let token;
-  if (req.headers.authorization?.startsWith('Bearer')) {
-    token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies.jwt) {
-    token = req.cookies.jwt || null;
-  }
+// exports.protect = catchAsync(async (req, res, next) => {
+//   // 1) Getting token and check if it's there
+//   let token;
+//   if (req.headers.authorization?.startsWith('Bearer')) {
+//     token = req.headers.authorization.split(' ')[1];
+//   } else if (req.cookies.jwt) {
+//     token = req.cookies.jwt || null;
+//   }
 
 //   if (!token)
 //     return next(
 //       new AppError('You are not logged in! Please login to get access', 401)
 //     );
 
-  // 2) Verification token
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+// 2) Verification token
+// const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
 //   // 3) Check if user still exists
 //   const currentUser = await User.findById(decoded.id);
